1a1fa25219582d4f6d6e18e875a8b5638f35d040
Michael Kevin Victor <me@mikevictor.me>
2016-04-10 00:40:25 +0530

    Update Ruby Syntax to >1.9

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,7 +4,7 @@ class CartsController < ApplicationController
     @carts = Cart.all
   end
 
-  def show        
+  def show
     @cart = current_cart
   end
 
@@ -30,25 +30,25 @@ class CartsController < ApplicationController
   end
 
   def payu_return
-    notification = PayuIndia::Notification.new(request.query_string, options = {:key => 'YOUR_KEY', :salt => 'YOUR_SALT', :params => params})
+    notification = PayuIndia::Notification.new(request.query_string, options = {key: 'YOUR_KEY', salt: 'YOUR_SALT', params: params})
 
     @cart = Cart.find(notification.invoice) # notification.invoice is order id/cart id which you have submited from payment direction page.
 
-    if notification.acknowledge      
+    if notification.acknowledge
       begin
-        if notification.complete?          
+        if notification.complete?
           @cart.status = 'success'
           @cart.purchased_at = Time.now
-          @order = Order.create(:total => notification.gross, :card_holder_name => params['name_on_card'], :order_number => notification.invoice)
+          @order = Order.create(total: notification.gross, card_holder_name: params['name_on_card'], order_number: notification.invoice)
           reset_session
           redirect_to @order
-        else          
+        else
           @cart.status = "failed"
-          render :text => "Order Failed! #{notification.message}"
+          render text: "Order Failed! #{notification.message}"
         end
       ensure
         @cart.save
       end
-    end    
+    end
   end
 end
